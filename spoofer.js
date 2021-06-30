6f76ec821242b14ed972563d22e884f4d60a680d
winstxnhdw <winstxnhdw@gmail.com>
2021-07-01 01:41:16 +0800

    auto date

---
+++ spoofer.js
@@ -0,0 +1,33 @@
+const d = new Date()
+const day = d.getDay()
+const date = d.getDate()
+const month = d.getMonth()
+const year = d.getFullYear()
+
+const days = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+]
+
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+]
+
+const dateString = `${days[day]}, ${date} ${months[month]} ${year}\n3:00 PM (1 hour 30 minutes)`
+document.getElementById('datetime').innerText = dateString
